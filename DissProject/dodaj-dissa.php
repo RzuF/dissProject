8e5359c5213c9ecb54b00b84871671370342f85f
Stosiu <aleksander.stos@gmial.com>
2015-06-22 23:28:04 +0200

    Template fixes

--- DissProject/dodaj-dissa.php
+++ DissProject/dodaj-dissa.php
@@ -16,12 +16,35 @@ include_once('config.php');
 </head>
 <body>
     <?php include_once("nav.html"); ?>
-    <form action='add.php' method='post' id='add'>
-        Tytu≥: <input type="text" name="dissName" id="dissName"></br>
-        Tekst: <textarea type="text" name="dissText" id="dissText"></textarea></br>
-        èrÛd≥o (opcjonalnie): <input type="text" name="dissSource" id="dissSource"></br>
-        <input type='submit' value='Dodaj' name='submit' id='submit'>
-    </form>
+
+    <div class="add-note">
+    	<div class="page-header">
+        <h1>Dodaj dissa:</h1>
+    	</div>
+    	<div class="alert alert-info" role="alert">Gdy dodajesz dissa będąc zajerestrowanym masz możliwość podlądu jego ocen i komentarzy. Te kilka z wielu możliwości zajerestrowanych użytkowników. Jeśli chcesz dowiedzieć się więcej <b><a href="#" class="alert-link">kliknij.</a></b></div>
+
+    	<form action='add.php' method='post' id='add'>
+    		<div class="form-group">
+            	<label for="word" class="control-label">Tytuł:</label>
+            	<input type="text" id="word" class="form-control" name="dissName" id="dissName">
+            </div>
+            <div class="form-group">
+            	<label for="word" class="control-label">Żródło:</label>
+            	<input type="text" class="form-control">
+            </div>
+            <div class="form-group">
+	            <label for="word" class="control-label">Tagi:</label>
+	            <input type="text" class="form-control">
+	        </div>
+	        <div class="form-group">
+	            <label for="mainText" class="control-label" name="dissText" id="dissText">Tekst:</label>
+	            <textarea class="form-control"></textarea>
+	        </div>
+	        <div class="form-group">
+	        	<button type="submit" name"submit" id="submit" class="form-control btn btn-default">Dodaj</button>
+	        </div>
+        </form>
+	</div> <!-- END NOTE -->
     <?php include_once("resources.html"); ?>
 </body>
 </html>
--- DissProject/index.php
+++ DissProject/index.php
@@ -13,7 +13,6 @@ include_once('config.php');
 	<title>DissProject</title>
 	<link rel="stylesheet" type="text/css" href="template/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="template/css/style.css">
-	<link rel="stylesheet" href="template/css/buttons.css">
 </head>
 <body>
     <?php include_once("nav.html"); ?>
@@ -21,85 +20,129 @@ include_once('config.php');
     	<div class="space"></div>
 
     	<div class="note">
+    		<!-- Note description -->
 			<div class="row note-title">
 				<div class="col-lg-8">
 					<div class="note-title-title">Testowy tytuł</div>
 				</div>
 				<div class="col-lg-4">
-					<div class="note-title-comments">65</div>
+					<div class="note-title-comments"><a href="#"><i class="fa fa-comments"></i> 65</a></div>
 				</div>
 			</div>
-			<div class="note-copright">Marcin Konieczny</div>
+
+			<div class="row">
+				<div class="col-lg-8">
+					<div class="note-copright"><i class="fa fa-user"></i> Marcin Konieczny</div>
+				</div>
+				<div class="col-lg-4">
+					<div class="note-date"> <i class="fa fa-calendar"></i> 25.06.2015</div>
+				</div>
+			</div>
+
+			<!-- Note image -->
+			<img src="getImage.php?id=1" class="img-responsive img-rounded">
+
+			<!-- Note rank -->
+			<div class="note-rank">
+				<div class="input-group">
+         			<span class="input-group-btn">
+              			<button type="button" class="btn btn-danger btn-number"  data-type="minus" data-field="quant[2]">
+                			<span class="fa fa-minus"></span>
+              			</button>
+          			</span>
+          			<input type="text" name="quant[2]" class="form-control input-number" value="0" min="0" max="100" disabled>
+          			<span class="input-group-btn">
+              			<button type="button" class="btn btn-success btn-number" data-type="plus" data-field="quant[2]">
+                  	<span class="fa fa-plus"></span>
+              			</button>
+          			</span>
+      			</div>
+			</div>
+		</div> <!-- END NOTE -->
+
+		<div class="space"></div>
+
+		<div class="note">
+    		<!-- Note description -->
+			<div class="row note-title">
+				<div class="col-lg-8">
+					<div class="note-title-title">Testowy tytuł</div>
+				</div>
+				<div class="col-lg-4">
+					<div class="note-title-comments"><a href="#"><i class="fa fa-comments"></i> 65</a></div>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-lg-8">
+					<div class="note-copright"><i class="fa fa-user"></i> Marcin Konieczny</div>
+				</div>
+				<div class="col-lg-4">
+					<div class="note-date"> <i class="fa fa-calendar"></i> 25.06.2015</div>
+				</div>
+			</div>
+
+			<!-- Note image -->
+			<img src="getImage.php?id=2" class="img-responsive img-rounded">
+
+			<!-- Note rank -->
+			<div class="note-rank">
+				<div class="input-group">
+         			<span class="input-group-btn">
+              			<button type="button" class="btn btn-danger btn-number"  data-type="minus" data-field="quant[2]">
+                			<span class="fa fa-minus"></span>
+              			</button>
+          			</span>
+          			<input type="text" name="quant[2]" class="form-control input-number" value="0" min="0" max="100" disabled>
+          			<span class="input-group-btn">
+              			<button type="button" class="btn btn-success btn-number" data-type="plus" data-field="quant[2]">
+                  	<span class="fa fa-plus"></span>
+              			</button>
+          			</span>
+      			</div>
+			</div>
+		</div> <!-- END NOTE -->
+
+		<div class="space"></div>
+
+		<div class="note">
+    		<!-- Note description -->
+			<div class="row note-title">
+				<div class="col-lg-8">
+					<div class="note-title-title">Testowy tytuł</div>
+				</div>
+				<div class="col-lg-4">
+					<div class="note-title-comments"><a href="#"><i class="fa fa-comments"></i> 65</a></div>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-lg-8">
+					<div class="note-copright"><i class="fa fa-user"></i> Marcin Konieczny</div>
+				</div>
+				<div class="col-lg-4">
+					<div class="note-date"> <i class="fa fa-calendar"></i> 25.06.2015</div>
+				</div>
+			</div>
+
+			<!-- Note image -->
 			<img src="getImage.php?id=3" class="img-responsive img-rounded">
+
+			<!-- Note rank -->
 			<div class="note-rank">
-				<button class="button button-primary button-circle button-small button-longshadow"><i class="fa fa-plus"></i></button>
-				<button class="button button-3d button-box button-small button-jumbo"><i class="fa fa-plus"></i></button>
-				<button class="button button-circle button-tiny"><i class="fa fa-plus"></i></button>
-				 564 
-				<button class="button button-primary button-circle button-small button-longshadow"><i class="fa fa-minus"></i></button>
-				<button class="button button-3d button-box button-small button-jumbo"><i class="fa fa-minus"></i></button>
-				<button class="button button-circle button-tiny"><i class="fa fa-minus"></i></button>
-			</div>
-
-		</div> <!-- END NOTE -->
-
-		<div class="space"></div>
-
-		<div class="note">
-			<div class="row note-title">
-				<div class="col-lg-8">
-					<div class="note-title-title">Testowy tytuł</div>
-				</div>
-				<div class="col-lg-4">
-					<div class="note-title-comments">65</div>
-				</div>
-			</div>
-			<div class="note-copright">Marcin Konieczny</div>
-			<img src="getImage.php?id=1" class="img-responsive img-rounded">
-			<div class="note-rank">
-				<button type="button" class="btn btn-default">+</button>
-				564
-				<button type="button" class="btn btn-default">-</button>
-			</div>
-		</div> <!-- END NOTE -->
-
-		<div class="space"></div>
-
-		<div class="note">
-			<div class="row note-title">
-				<div class="col-lg-8">
-					<div class="note-title-title">Testowy tytuł</div>
-				</div>
-				<div class="col-lg-4">
-					<div class="note-title-comments">65</div>
-				</div>
-			</div>
-			<div class="note-copright">Marcin Konieczny</div>
-			<img src="getImage.php?id=1" class="img-responsive img-rounded">
-			<div class="note-rank">
-				<button type="button" class="btn btn-default">+</button>
-				564
-				<button type="button" class="btn btn-default">-</button>
-			</div>
-		</div> <!-- END NOTE -->
-
-		<div class="space"></div>
-
-		<div class="note">
-			<div class="row note-title">
-				<div class="col-lg-8">
-					<div class="note-title-title">Testowy tytuł</div>
-				</div>
-				<div class="col-lg-4">
-					<div class="note-title-comments">65</div>
-				</div>
-			</div>
-			<div class="note-copright">Marcin Konieczny</div>
-			<img src="getImage.php?id=1" class="img-responsive img-rounded">
-			<div class="note-rank">
-				<button type="button" class="btn btn-default">+</button>
-				564
-				<button type="button" class="btn btn-default">-</button>
+				<div class="input-group">
+         			<span class="input-group-btn">
+              			<button type="button" class="btn btn-danger btn-number"  data-type="minus" data-field="quant[2]">
+                			<span class="fa fa-minus"></span>
+              			</button>
+          			</span>
+          			<input type="text" name="quant[2]" class="form-control input-number" value="0" min="0" max="100" disabled>
+          			<span class="input-group-btn">
+              			<button type="button" class="btn btn-success btn-number" data-type="plus" data-field="quant[2]">
+                  	<span class="fa fa-plus"></span>
+              			</button>
+          			</span>
+      			</div>
 			</div>
 		</div> <!-- END NOTE -->
 
@@ -129,5 +172,6 @@ include_once('config.php');
     </div>
 
     <?php include_once("resources.html"); ?>
+    <script src="template/js/rank.js"></script>
 </body>
 </html>
