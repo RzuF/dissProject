e4db3e0125165527362a4032e23b2cdeff3555bf
RzuF <rzuf22@gmail.com>
2015-06-24 23:15:19 +0200

    Add getAll.php file

---
+++ DissProject/getAll.php
@@ -0,0 +1,30 @@
+<?php
+
+include_once('config.php');
+
+session_start();
+
+$sqlcon = mysql_connect(HOST, USER, PASS);
+if(!$sqlcon) echo 'Error: '.mysql_error();
+
+$blad = mysql_select_db(DB);
+if(!$blad) echo 'Error: '.mysql_error();
+
+if(isset($_GET['id']))
+{
+	$idreq = mysql_query("SELECT a.title, a.plus, a.minus, a.date, b.login, a.source FROM ".PREFIX."_notes a LEFT JOIN ".PREFIX."_users b ON a.author = b.id");
+	if(!$idreq) echo "Error: ".mysql_error();
+	else
+	{
+		$arr = array();
+		
+		while($req = mysql_fetch_assoc($idreq))
+		{
+			$arr[] = $req; 			
+		}
+		
+		echo $json_response = json_encode($arr);
+	}
+}
+
+?>
